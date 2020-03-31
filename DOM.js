90d5c5e1b25f4291fc733929207329ff1a529eb5
HakanTasdelen <hakanfbmer@outlook.com>
2020-04-01 00:17:54 +0300

    6. VİDEO

---
+++ DOM.js
@@ -0,0 +1,39 @@
+//Tüm Elemanları göstermek için 
+console.log(document.all);
+
+//Tüm Elemanların sayısını  göstermek için 
+console.log(document.all.length);
+
+//İstenilen elemanı göstermek için 
+console.log(document.all[0]);
+
+//Head kısmını göstermek için 
+console.log(document.head);
+
+
+//Body kısmını göstermek için 
+console.log(document.body);
+
+//Title kısmını göstermek için 
+console.log(document.title);
+
+//Uri kısmını göstermek için 
+console.log(document.documentURI);
+
+//URL kısmını göstermek için 
+console.log(document.URL);
+
+//Tüm linkleri göstermek için 
+console.log(document.anchors);
+
+//Domain kısmını göstermek için 
+console.log(document.domain);
+
+//Script'leri göstermek için 
+console.log(document.scripts);
+
+//Formları göstermek için 
+console.log(document.forms);
+
+//İstenilen formu göstermek için 
+console.log(document.forms[0]);
